daa4e8b346660955cdd32e46e8cfc9157a1ac50d
vu <vul@sendo.vn>
2026-09-23 00:02:04 +0700

    fix Certificate Thumbprint github

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -78,7 +78,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "data_lake" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  # GitHub Actions uses two possible certificates, include both to prevent OIDC errors
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
   tags            = local.common_tags
 }
 
